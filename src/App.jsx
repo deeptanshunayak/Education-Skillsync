87016c0f844ccc2442dfb9174672d93aaf433f61
deeptanshunayak <deeptanshunayak@gmail.com>
2024-03-08 13:42:28 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {Navbar,Home,About,Contact,Teacher,Footer,Course} from "./Components/index"
+
+function App() {
+  
+
+  return (
+   <div className="font-Poppins bg-Solitude">
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Course/>
+    <Teacher/>
+    <Contact/>
+    <Footer/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/Components/index.js
@@ -0,0 +1,8 @@
+import Home from "./container/Home"
+import About from "./container/About"
+import Course from "./container/Course/Course"
+import Contact from "./container/Contact"
+import Footer from "./container/Footer"
+import Navbar from "./Navbar/Navbar"
+import Teacher from "./container/Teacher"
+export {Navbar,Home,Teacher,About,Course,Contact,Footer}
